84ab683fd22adda44352d29ceebf771459d94d18
Levi Mann <lmann4@ksu.edu>
2017-02-02 14:22:23 -0600

    Made the file server dynamic

--- server.js
+++ server.js
@@ -11,14 +11,25 @@
 
  var stylesheet = fs.readFileSync("gallery.css");
 
- var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
+ var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg']
+
+function getImageNames(callback) {
+  fs.readdir('images/', function(err, filenames) {
+    if (err) {
+      callback(err, undefined);
+    }
+    else {
+      callback(false, filenames);
+    }
+  });
+};
 
  function serveImage(filename, req, res) {
     var body = fs.readFile('images/' + filename, function(err, body){
       if (err) {
-        res.statusCode = 500;
+        res.statusCode = 404;
         res.statusMessage = "whoops";
-        res.end("Silly me");
+        res.end("<h1>404: File Not Found!</h1>");
         console.error(err);
         return; //stop execution
       }
@@ -26,49 +37,53 @@
       res.end(body);
     });
  }
+
+function buildGallery(imageNames) {
+  var html = '<!doctype html>';
+  html += '<head>';
+  html += '  <title>Gallery</title>';
+  html += '  <link href="gallery.css" rel="stylesheet" type="text/css">';
+  html += '</head>';
+  html += '<body>';
+  html += '  <h1>Gallery</h1>';
+  html += `<div id="images">${imageNamesToTags(imageNames).join('')}</div>`;
+  html += '  <h1>Hello.</h1> Time is ' + Date.now();
+  html += '</body>';
+  return html;
+}
+
+function serveGallery(req, res) {
+  getImageNames(function (err, imageNames) {
+    if (err) {
+       res.statusCode = 500;
+       res.statusMessage = 'Server Error';
+       res.end();
+       return;
+    }
+      res.setHeader('Content-Type', 'text/html');
+      res.end(buildGallery(imageNames));
+  });
+}
+
+
+ function imageNamesToTags(filenames) {
+   return filenames.map(function (filename) {
+     return `<img src="${filename}" alt="${filename}">`;
+   });
+ }
                                     //  => is lambda notation for a function
  var server = http.createServer((req, res) => {
    switch (req.url) {
+     case '/':
      case '/gallery':
-       var gHtml = imageNames.map(function (filename) {
-         return '<img src="' + filename + '" alt="a fishing ace at work">';
-       }).join(' ');
-       var html = '<!doctype html>';
-       html += '<head>';
-       html += '  <title>Gallery</title>';
-       html += '  <link href="gallery.css" rel="stylesheet" type="text/css">';
-       html += '</head>';
-       html += '<body>';
-       html += '  <h1>Gallery</h1>';
-       html += gHtml;
-       html += '  <h1>Hello.</h1> Time is ' + Date.now();
-       html += '</body>';
-       res.setHeader('Content-Type', 'text/html');
-       res.end(html);
-       break;
-     case "/chess.jpg":
-       serveImage('chess.jpg', req, res);
-       break;
-     case "/fern.jpg":
-       serveImage('fern.jpg', req, res);
-       break;
-     case "/bubble.jpg":
-       serveImage('bubble.jpg', req, res);
-       break;
-     case "/ace.jpg":
-       serveImage('ace.jpg', req, res);
-       break;
-     case "/mobile.jpg":
-       serveImage('mobile.jpg', req, res);
+       serveGallery(req, res);
        break;
      case "/gallery.css":
        res.setHeader('Content-Type', 'text/css');
        res.end(stylesheet);
        break;
      default:
-        res.statusCode = 404;
-        res.statusMessage = "Not found"
-        res.end()
+        serveImage(req.url, req, res);
    }
  })
 
