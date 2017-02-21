f5084a1f583668895b93f3b3be982f4dd4204081
Levi Mann <lmann4@ksu.edu>
2017-02-21 14:18:05 -0600

    Added a js file and event handler to hide form

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ var port = 3000;
 
 /* load cached files */
 var config = JSON.parse(fs.readFileSync('config.json'));
-var stylesheet = fs.readFileSync('gallery.css');
+var stylesheet = fs.readFileSync('public/gallery.css');
+var script = fs.readFileSync('public/gallery.js');
 
 /* load templates */
 template.loadDir('templates');
@@ -162,6 +163,10 @@ function handleRequest(req, res) {
     case '/gallery.css':
       res.setHeader('Content-Type', 'text/css');
       res.end(stylesheet);
+      break;
+    case '/gallery.js':
+      res.setHeader('Content-Type', 'text/javascript');
+      res.end(script);
       break;
     default:
       serveImage(req.url, req, res);
